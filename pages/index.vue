9bac95bcd3e2edf023930f1ef51db058ef61575d
jha <jha@novicell.dk>
2018-09-16 17:41:18 +0200

    Removed templates from components

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,12 @@
 <template>
-  <component
-    v-if="componentTemplate"
-    :is="componentTemplate"/>
+  <div>
+    HomePage
+  </div>
 </template>
 
 <script>
-const components = {
-  HomePage: () => import('@/components/templates/HomePage/HomePage'),
-};
 
 export default {
-  components,
-  computed: {
-    componentTemplate() {
-      return 'HomePage'; // This is just a default for now
-    },
-  },
 };
 </script>
 
